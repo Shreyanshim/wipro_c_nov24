c728cee11b115ec892dcd11b56cc8a5940980a6d
Shreyanshim <87068610+Shreyanshim@users.noreply.github.com>
2024-11-27 20:24:44 +0530

    Add files via upload

---
+++ asi4_4e.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int size, i, j;
+
+    printf("Enter the size of the square (odd number): ");
+    scanf("%d", &size);
+
+    if (size % 2 == 0) {
+        printf("Please enter an odd number.\n");
+        return 1; 
+    }
+
+  
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+           
+            if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+                printf("*");
+            }
+            
+            else if (j == i || j == size - i - 1) {
+                printf("*");
+            } else {
+                printf(" "); 
+            }
+        }
+       
+        printf("\n");
+    }
+
+    return 0;
+}
